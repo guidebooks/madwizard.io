1d0f304b4ff589f37fd087fb2009d5605f8317d4
Nick Mitchell <nickm@us.ibm.com>
2023-01-25 11:25:56 -0500

    fix: small optimizations to page load
    1. don't bother rendering ProcessingBlock; this also avoids the UI glitch where `commentary -f ...` is briefly shown
    2. invoke `commentary` directly, rather than via `replay`

--- plugins/plugin-client-offline/src/index.tsx
+++ plugins/plugin-client-offline/src/index.tsx
@@ -43,7 +43,9 @@ export default function renderMain(props: KuiProps) {
       productName={productName}
       lightweightTables
       {...props}
-      commandLine={props.commandLine || autoplay.length === 0 ? [] : ["replay", "-r", ...autoplay]}
+      quietExecCommand={false}
+      loading={<div />}
+      commandLine={props.commandLine || autoplay.length === 0 ? [] : ["commentary", "--readonly", "-f", ...autoplay]}
     >
       <ContextWidgets>
         {/* widgets you want to appear flush left */}
